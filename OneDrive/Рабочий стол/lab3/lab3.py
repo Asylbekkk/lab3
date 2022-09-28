deb5808d751f65f9eabe7cefc24c62d7360d6084
Asylbekkk <arakhmanberdy@gmail.com>
2022-09-28 14:44:41 +0600

    Fourth commit

--- OneDrive/Рабочий стол/lab3/lab3.py
+++ OneDrive/Рабочий стол/lab3/lab3.py
@@ -1,4 +1,10 @@
-a = int(input())
-b = int(input())
-for i in range(a - (a + 1) % 2, b - b % 2, -2):
-    print(i, end=' ')
+n = int(input())
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+# можно доказать формулу:
+# sum == n * (n + 1) // 2
+# но мы посчитаем это значение циклом
+for i in range(n - 1):
+    sum -= int(input())
+print(sum)
